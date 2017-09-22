9e62a60e1e9a6438dfcef19ae9a15647d51db98d
Kortoshkina Ekaterina <liberty_12@mail.ru>
2017-09-22 19:11:19 +0300

    Update HelloWorldProgram.cs

--- HelloWorldProgram.cs
+++ HelloWorldProgram.cs
@@ -10,7 +10,7 @@ namespace Hello_world
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, world!");
+            Console.WriteLine("Hello, User!");
             Console.ReadLine();
         }
     }
